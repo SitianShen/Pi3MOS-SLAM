84b1b6061f08cbb35bf6ad34dcb44fe23f8f7285
sitian <1215665036@qq.com>
2026-02-13 17:15:25 +0000

    re-fixing pi3mos_slam adaptations

--- dpvo/fastba/ba.py
+++
@@ -1,8 +0,0 @@
-import torch
-import cuda_ba
-
-neighbors = cuda_ba.neighbors
-reproject = cuda_ba.reproject
-
-def BA(poses, patches, intrinsics, target, weight, prior_depth, prior_weights, lmbda, ii, jj, kk, t0, t1, M, iterations, eff_impl=False, use_cov_adaptive=True, cov_steepness=10.0, cov_threshold=2.0):
-    return cuda_ba.forward(poses.data, patches, intrinsics, target, weight, prior_depth, prior_weights, lmbda, ii, jj, kk, M, t0, t1, iterations, eff_impl, use_cov_adaptive, cov_steepness, cov_threshold)
--- scripts/download_model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://www.dropbox.com/s/nap0u8zslspdwm4/models.zip && unzip models.zip
-mv dpvo.pth checkpoints/dpvo.pth
-rm models.zip
